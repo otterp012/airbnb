ff6b1cd582da715e76daf1d0bde7d011c743c13b
happyGyu <happyboyto.dev@gmail.com>
2022-05-23 19:08:35 +0900

    build: set lint, prettier config and handle errors
    Co-authored-by: name <otterp012@gmail.com>

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,27 +1,27 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
     open: false,
     hot: true,
     compress: true,
     port: 8081,
     historyApiFallback: true,
-    liveReload: true
+    liveReload: true,
   },
   output: {
-    filename: "[name].[contenthash].js",
-    publicPath: "/"
+    filename: '[name].[contenthash].js',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/i,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 });
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,26 +1,26 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+const { merge } = require('webpack-merge');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common');
 
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "cheap-module-source-map",
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "./",
-    clean: true
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: './',
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   plugins: [new MiniCssExtractPlugin()],
   optimization: {
@@ -28,13 +28,13 @@ module.exports = merge(common, {
     minimize: true,
     minimizer: [
       new TerserPlugin({ terserOptions: { compress: { drop_console: true } } }),
-      new CssMinimizerPlugin()
+      new CssMinimizerPlugin(),
     ],
-    splitChunks: { chunks: "all" }
+    splitChunks: { chunks: 'all' },
   },
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  }
+    maxAssetSize: 512000,
+  },
 });
