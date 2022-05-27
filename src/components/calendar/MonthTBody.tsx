5cb3128a7ac16ee47d7fb99a20603dcf01eac40a
otterp012 <otterp012@gmail.com>
2022-05-27 11:13:28 +0900

    feat: add calendar events Co-authored-by: name <happyboyto.dev@gmail.com>

--- src/components/calendar/MonthTBody.tsx
+++ src/components/calendar/MonthTBody.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { getFirstDayIdx, getLastDate, splitDatesToWeeks } from '../../util/calenderUtil';
+
+import {
+  getFirstDayIdx,
+  getLastDate,
+  splitDatesToWeeks,
+} from '../../util/calenderUtil';
 import WeekTableRow from './WeekTRow';
 
-const CalendarMonthTable = ({ year, month }: { year: number; month: number }) => {
+const CalendarMonthTable = ({
+  year,
+  month,
+}: {
+  year: number;
+  month: number;
+}) => {
   const firstDayIdx = getFirstDayIdx(year, month);
   const lastDate = getLastDate(year, month);
   const weekArr = splitDatesToWeeks(firstDayIdx, lastDate);
--- src/components/calendar/MonthTable.tsx
+++ src/components/calendar/MonthTable.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import MonthTHead from './MonthTHead';
 import MonthTBody from './MonthTBody';
+import CalendarContext from '../../store/CalendarContext';
 
-const MonthTable = ({ year, month }: { year: number; month: number }) => (
-  <table>
-    <MonthTHead year={year} month={month} />
-    <MonthTBody year={year} month={month} />
-  </table>
-);
+const MonthTable = ({ year, month }: { year: number; month: number }) => {
+  const { dispatchCheckedDate } = useContext(CalendarContext);
+
+  const onMouseLeaveHandler = () => {
+    dispatchCheckedDate({ type: 'HOVER', payload: null });
+  };
+
+  return (
+    <StyledTable onMouseLeave={onMouseLeaveHandler}>
+      <MonthTHead year={year} month={month} />
+      <MonthTBody year={year} month={month} />
+    </StyledTable>
+  );
+};
+
+const StyledTable = styled.table`
+  -webkit-user-select: none;
+`;
 
 export default MonthTable;
