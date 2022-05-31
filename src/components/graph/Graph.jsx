261de21cee0322d0b0d399af5886cef3cf8df453
otterp012 <otterp012@gmail.com>
2022-05-31 18:07:56 +0900

    [fix]: revises graph logic, utils

--- src/components/graph/Graph.jsx
+++ src/components/graph/Graph.jsx
@@ -1,7 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { pathCoords } from '../../util/graphUtil';
 
 const Graph = () => {
+  // todo 드래그
+  const [left, setLeft] = useState(0);
+  const dragStartHandler = (e) => {
+    setLeft(e.clientX - 350);
+  };
+
+  const dragHander = (e) => {
+    setLeft(e.clientX + e.offsetX - 350);
+  };
+
   return (
     <Wrapper>
       <GraphSvg width="400" height="100" viewBox="0 0 400 100">
@@ -19,13 +30,20 @@ const Graph = () => {
         <path
           mask="url(#mask)"
           clipPath="url(#clippath)"
-          d={ddd}
+          d={pathCoords}
           stroke="red"
           strokeWidth="0.4"
           fill="black"
         />
       </GraphSvg>
-      <Slider />
+      <Slider>
+        <Btn
+          draggable
+          onDragEnd={dragStartHandler}
+          onDrag={dragHander}
+          left={left}
+        />
+      </Slider>
     </Wrapper>
   );
 };
@@ -35,12 +53,12 @@ const Btn = styled.div`
   height: 30px;
   background: red;
   position: absolute;
-  left: 100px;
-  top: 100px;
+  left: ${({ left }) => `${left}px`};
+  top: 20px;
 `;
-// left: ${({ left }) => left + 'px'};
-// top: ${({ y }) => y + 'px'};
+
 const Slider = styled.div`
+  position: relative;
   width: 100%;
   height: 50px;
   background: blue;
@@ -57,63 +75,5 @@ const Wrapper = styled.div`
 const GraphSvg = styled.svg`
   transform: scaleY(-1);
 `;
-const smoothing = 0.2;
-
-const points = [
-  [0, 0],
-  [5, 10],
-  [10, 40],
-  [40, 30],
-  [60, 5],
-  [90, 45],
-  [120, 10],
-  [150, 45],
-  [200, 10],
-  [300, 10],
-  [350, 0],
-];
-
-const line = (pointA, pointB) => {
-  const lengthX = pointB[0] - pointA[0];
-  const lengthY = pointB[1] - pointA[1];
-  return {
-    length: Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)),
-    angle: Math.atan2(lengthY, lengthX),
-  };
-};
-
-const controlPoint = (current, previous, next, reverse) => {
-  const p = previous || current;
-  const n = next || current;
-
-  const o = line(p, n);
-
-  const angle = o.angle + (reverse ? Math.PI : 0);
-  const length = o.length * smoothing;
-  const x = current[0] + Math.cos(angle) * length;
-  const y = current[1] + Math.sin(angle) * length;
-  return [x, y];
-};
-
-const bezierCommand = (point, i, a) => {
-  const cps = controlPoint(a[i - 1], a[i - 2], point);
-  const cpe = controlPoint(point, a[i - 1], a[i + 1], true);
-  return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`;
-};
-
-const svgPath = (points, command) => {
-  // build the d attributes by looping over the points
-  const d = points.reduce(
-    (acc, point, i, a) =>
-      i === 0 ? `M ${point[0]},${point[1]}` : `${acc} ${command(point, i, a)}`,
-    '',
-  );
-  return d;
-};
-
-const ddd = points.reduce((acc, cur, i, a) => {
-  if (!i) return `M ${cur[0]},${cur[1]}`;
-  return `${acc} ${bezierCommand(cur, i, a)}`;
-}, '');
 
 export default Graph;
