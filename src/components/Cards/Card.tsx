c6a164502e1f95fc90b15349c28e5d82192663d5
otterp012 <otterp012@gmail.com>
2022-06-08 14:25:43 +0900

    [feat]: serachPage 카드 컴포넌트 구성 및 데이터 페치 추가

---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import CardText from './CardsTexts';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+const Card = ({ img, price, name }) => {
+  const pr = 1;
+  return (
+    <CardContainer>
+      <CardImage src={img} />
+      <CardText price={price} name={name} />
+      <HeartIcon />
+    </CardContainer>
+  );
+};
+
+export default Card;
+
+const CardContainer = styled.div`
+  position: relative;
+  ${({ theme }) => theme.mixin.flexMixin('row', 'center', 'center')}
+  padding: 20px 0;
+  width: 100%;
+  height: 30%;
+  box-sizing: border-box;
+  border-bottom: 1px solid #e0e0e0;
+`;
+
+const CardImage = styled.img`
+  width: 330px;
+  height: 200px;
+  background: blue;
+  border-radius: 10px;
+  margin-right: 10px;
+`;
+
+const HeartIcon = styled(FavoriteBorderIcon)`
+  position: absolute;
+  top: 30px;
+  right: 10px;
+`;
